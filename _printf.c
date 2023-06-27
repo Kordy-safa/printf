c88235559bd0ae735119f7d48b09cce3b444d828
Kimathi isaac <Isaackimathi@gmail.com>
2023-06-28 01:07:28 +0300

    printf project

--- _printf.c
+++ _printf.c
@@ -3,43 +3,51 @@
 /**
  *_printf - function that produces output according to a format
  *@format : character string
- *return: integer
+ *return: integer 0 (success)
  */
-
 int _printf(const char *format, ...)
 {
-       unsigned int q = 0, s_value = 0;
-       va_list saf;
+	unsigned int q = 0, value = 0;
+	va_list saf;
 
-for (;format[q] != '\0'; q++)
- {
- if (format [q] != '%')
- {
-  _putchr(format[q]);
- }
- else if (format[q+1] == 'c')
-                {
-                        _putchr(va_arg(saf, int));
-                        q++;
-                }
- else if (format[q+1] == 's')
-                {
-                        int s_val = _put_str(va_arg (saf, char *));
-                        q++;
-                        s_value += s_val;
-                }
- else if (format[q+1] == '%')
-                {
-                        _putchr('%');
-                        q++;
-                }
- else if ((format[q+1] == 'd') || (format[q+1] == 'i'))
-                {
-                        s_value += _put_int(va_arg(saf, int));
-                        q++;
-                }
- s_value += 1;
-}
+	va_start(saf, format);
 
-return (0);
+	for (; format[q] != '\0'; format++)
+	{
+		if (format[q] != '%')
+		{
+			if (format[q + 1] == 'c')
+			{
+			int z = (va_arg(saf, int));
+
+			_putchr(z);
+			q++;
+			}
+		else if (format[q + 1] == 's')
+		{
+			char *s = (va_arg(saf, char *));
+
+			while (*s != '\0')
+			{
+				_putchr(*s++);
+				s++;
+				q++;
+			}
+		}
+		}
+		else if (format[q + 1] == '%')
+		{
+			_putchr('%');
+			q++;
+		}
+		else
+		{
+			_putchr(format[q + 1]);
+			q++;
+		}
+		value++;
+	}
+	va_end(saf);
+
+	return (value);
 }
