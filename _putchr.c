d95c6b73eb63b4adcd8b06152335ea5c433d7f75
Kordy-safa <safaelkordy@gmail.com>
2023-06-28 00:18:37 -0700

    printf project

--- _putchr.c
+++ _putchr.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
+/**
+ * _putchr - writes a character to the standard output
+ * @z: character to be written
+ * Return: On success, returns the number of characters written.
+ * On error, returns -1.
+ */
 int _putchr(char z)
 {
-        return write(1, &z, 1);
+	return (write(1, &z, 1));
 }
